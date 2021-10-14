f787521b043dd7defb662d740774e5989e386cbf
Pengu <59419974+Pengu01@users.noreply.github.com>
2021-10-14 14:01:49 +0200

    lägger till class
    som fixar en deck lista

--- BlackJackGame/Program.cs
+++ BlackJackGame/Program.cs
@@ -9,4 +9,18 @@ namespace BlackJackGame
             Console.WriteLine("Hello World!");
         }
     }
+     class deck
+    {
+        List<int> cards = new List<int>();
+        public void deckAdd()
+        {
+            for(int i = 2; i <= 10; i++)
+            {
+                for(int j = 0; j < 4; j++)
+                {
+                    cards.Add(i);
+                }
+            }
+        }
+    }
 }
