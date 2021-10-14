fd23398d4b2e998ebd12333756669c4a1db9a127
ErikNoren05 <91731222+ErikNoren05@users.noreply.github.com>
2021-10-14 14:16:07 +0200

    Update Program.cs

--- BlackJackGame/Program.cs
+++ BlackJackGame/Program.cs
@@ -15,6 +15,9 @@ namespace BlackJackGame
             Console.Write("vänligen skriv in vad ess ska vara värt(1 eller 11):");
             ess= Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("bra, ess är nu värt" + ess);
+            Console.WriteLine("då kör vi");
+            //spelare och bot får två kort
+            
         }
     }
     class deck
