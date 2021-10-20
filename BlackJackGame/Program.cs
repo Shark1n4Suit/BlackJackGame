70a7fd0bbf344cc925a20ef5ba2e2aecc6b1c0ea
Pengu <59419974+Pengu01@users.noreply.github.com>
2021-10-20 13:26:47 +0200

    Update Program.cs

--- BlackJackGame/Program.cs
+++ BlackJackGame/Program.cs
@@ -16,6 +16,7 @@ namespace BlackJackGame
             while (true)
             {
                 bj.deckAdd();
+                Console.WriteLine("Balance: " + money);
                 Console.WriteLine("Enter bet value");
                 bet = Convert.ToInt32(Console.ReadLine());
                 money -= bet;
@@ -40,9 +41,66 @@ namespace BlackJackGame
                     {
                         player.Remove(player.IndexOf(11));
                         player.Add(1);
-                    }
-                    else Console.WriteLine("Bust!"); continue;
+                    } 
+                    else Console.WriteLine("Bust!"); bj.deckRemove(); continue;
                 }
+                bool hitstand = true;
+                while(hitstand == true)
+                {
+                    Console.WriteLine("Hit/Stand");
+                    string input = Console.ReadLine();
+                    if (input == "Hit")
+                    {
+                        player.Add(bj.draw());
+                        Console.WriteLine("Card is " + player[2] + "\nTotal is " + player.Sum(x => Convert.ToInt32(x)));
+                        if (player.Sum(x => Convert.ToInt32(x)) > 21)
+                        {
+                            if (player.Contains(11))
+                            {
+                                player.Remove(player.IndexOf(11));
+                                player.Add(1);
+                            } 
+                            else Console.WriteLine("Bust!"); bj.deckRemove(); continue;
+                        }
+                    }
+                    else if (input == "Stand")
+                    {
+                        hitstand = false;
+                    }
+                    else Console.WriteLine("Invalid input, reset");
+                }
+                bool dealerturn = true;
+                while(dealerturn == true)
+                {
+                    if (dealer.Sum(x => Convert.ToInt32(x)) > 16)
+                    {
+                        dealer.Add(bj.draw());
+                        if (dealer.Sum(x => Convert.ToInt32(x)) > 21)
+                        {
+                            if (dealer.Contains(11))
+                            {
+                                dealer.Remove(dealer.IndexOf(11));
+                                dealer.Add(1);
+                            }
+                            else Console.WriteLine("Dealer bust!"); money += bet + bet; bj.deckRemove(); continue;
+                        }
+                    }
+                    else dealerturn = false;
+                }
+                if (dealer.Sum(x => Convert.ToInt32(x)) < player.Sum(x => Convert.ToInt32(x)))
+                {
+                    Console.WriteLine("Player win!");
+                    money += bet + bet;
+                    bj.deckRemove();
+                    continue;
+                }
+                else if (dealer.Sum(x => Convert.ToInt32(x)) > player.Sum(x => Convert.ToInt32(x)))
+                {
+                    Console.WriteLine("Dealer win!");
+                    bj.deckRemove();
+                    continue;
+                }
+                else Console.WriteLine("Tie!"); money += bet; bj.deckRemove(); continue;
             }
         }
     }
